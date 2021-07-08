4d77b36e7512b0304847fc8507ae225ac1c5306e
Stuart McCann <smccann74@qub.ac.uk>
2021-07-08 11:04:35 +0100

    JobProfile Add

---
+++ Prototype/Models/JobProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prototype.Models
+{
+    public class JobProfile
+    {
+
+        public int JobID { get; set; }
+        public String JobDescription { get; set; }
+        public double UpperRate { get; set; }
+        public double LowerRate { get; set; }
+        public String JobTitle { get; set; }
+        public String CompanyName { get; set; }
+        public int Duration { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public double Rating { get; set; }
+    }
+}
