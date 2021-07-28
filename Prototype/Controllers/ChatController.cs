7203c5874d9334b9838f7f96bfdb71448d2a2483
Stuart McCann <smccann74@qub.ac.uk>
2021-07-28 22:06:10 +0100

    Chat index functionality

--- Prototype/Controllers/ChatController.cs
+++ Prototype/Controllers/ChatController.cs
@@ -14,30 +14,38 @@ namespace Prototype.Controllers
     public class ChatController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly ApplicationDbContext _context;
-        public ChatController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        private readonly ApplicationDbContext _db;
+        public ChatController(UserManager<ApplicationUser> userManager, ApplicationDbContext applicationDbContext)
         {
             _userManager = userManager;
-            _context = context;
+            _db = applicationDbContext;
         }
 
+        public IActionResult Index()
+        {
+            var userId = User.Claims.Where(a => a.Type == ClaimTypes.NameIdentifier).Select(a => a.Value).FirstOrDefault();
+            IEnumerable<ApplicationUser> users = _db.Users.Where(user => user.Id != userId).ToList();
 
+            return View(users); 
+        }
 
         //[HttpGet("users")]
-        public async Task<IActionResult> GetUsersAsync()
+        public  List<ApplicationUser> GetUsers()
         {
 
             //add query here to get only those who can message 
             var userId = User.Claims.Where(a => a.Type == ClaimTypes.NameIdentifier).Select(a => a.Value).FirstOrDefault();
-            var allUsers = await _context.Users.Where(user => user.Id != userId).ToListAsync();
-            return Ok(allUsers);
+            var allUsers =  _db.Users.Where(user => user.Id != userId).ToList();
+            return  allUsers;
         }
+
+        
 
 
         //[HttpGet("users/{userId}")]
         public async Task<IActionResult> GetUserDetailsAsync(string userId)
         {
-            var user = await _context.Users.Where(user => user.Id == userId).FirstOrDefaultAsync();
+            var user = await _db.Users.Where(user => user.Id == userId).FirstOrDefaultAsync();
             return Ok(user);
         }
 
@@ -49,16 +57,16 @@ namespace Prototype.Controllers
             var userId = User.Claims.Where(a => a.Type == ClaimTypes.NameIdentifier).Select(a => a.Value).FirstOrDefault();
             message.FromUserId = userId;
             message.CreatedDate = DateTime.Now;
-            message.ToUser = await _context.Users.Where(user => user.Id == message.ToUserId).FirstOrDefaultAsync();
-            await _context.ChatMessages.AddAsync(message);
-            return Ok(await _context.SaveChangesAsync());
+            message.ToUser = await _db.Users.Where(user => user.Id == message.ToUserId).FirstOrDefaultAsync();
+            await _db.ChatMessages.AddAsync(message);
+            return Ok(await _db.SaveChangesAsync());
         }
 
-        //[HttpGet("{contactId}")]
-        public async Task<IActionResult> GetConversationAsync(string contactId)
+        //need to pass id for cand/client here 
+        public  List<ChatMessage> GetConversation(string contactId)
         {
             var userId = User.Claims.Where(a => a.Type == ClaimTypes.NameIdentifier).Select(a => a.Value).FirstOrDefault();
-            var messages = await _context.ChatMessages
+            var messages = _db.ChatMessages
                     .Where(h => (h.FromUserId == contactId && h.ToUserId == userId) || (h.FromUserId == userId && h.ToUserId == contactId))
                     .OrderBy(a => a.CreatedDate)
                     .Include(a => a.FromUser)
@@ -72,8 +80,21 @@ namespace Prototype.Controllers
                         ToUserId = x.ToUserId,
                         ToUser = x.ToUser,
                         FromUser = x.FromUser
-                    }).ToListAsync();
-            return Ok(messages);
+                    }).ToList();
+
+            //var messages = (from c in _db.ChatMessages
+            //               where c.FromUserId == userId || c.ToUserId == userId
+            //               select new ChatMessage
+            //               {
+            //                   Id = c.Id, 
+            //                   FromUserId = c.FromUserId, 
+            //                   ToUserId = c.ToUserId, 
+            //                   Message = c.Message, 
+            //                   CreatedDate = c.CreatedDate, 
+            //                   ToUser = c.ToUser, 
+            //                   FromUser = c.FromUser
+            //               }).ToList(); 
+            return messages;
         }
 
     }
